b20080ff283c950ed342d734c70a6d15b13f3dd4
Halit Akca <halitakca070@gmail.com>
2023-07-05 20:47:18 +0300

    1- ViewBinding feature has been added to the project. 2- UI has been designed.

--- app/src/main/java/com/halitakca/kotlininstagram/MainActivity.kt
+++ app/src/main/java/com/halitakca/kotlininstagram/MainActivity.kt
@@ -2,14 +2,27 @@ package com.halitakca.kotlininstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.halitakca.kotlininstagram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
 
 
+    }
+
+
+    fun signInClicked(view: View){
+
+    }
+
+    fun signUpClicked(view: View){
 
     }
 
--- app/src/main/java/com/halitakca/kotlininstagram/UploadActivity.kt
+++ app/src/main/java/com/halitakca/kotlininstagram/UploadActivity.kt
@@ -2,10 +2,25 @@ package com.halitakca.kotlininstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.halitakca.kotlininstagram.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+
+    fun upload(view: View){
+
+    }
+
+    fun selectImage(view: View){
+
     }
 }
